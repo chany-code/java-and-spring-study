83f5b82f0b3cc898c0f09f19ea935313ea71c1cd
chany-code <channn926@gmail.com>
2022-09-10 01:04:14 +0900

    Adapter pattern

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -1,5 +1,6 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
@@ -7,7 +8,7 @@ import com.company.design.singleton.SocketClient;
 public class Main {
 
     public static void main(String[] args) {
-
+        /* Singleton
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -16,5 +17,24 @@ public class Main {
 
         System.out.println("두개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));
+         */
+
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        //connect(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        //connect(airConditioner);
+        connect(airAdapter);
+    }
+
+    // 콘센트
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
     }
 }
