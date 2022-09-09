5593ab191c55775433a2d2a90c7ad16ad2f16f5a
chany-code <channn926@gmail.com>
2022-09-10 01:24:31 +0900

    Proxy pattern

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -1,14 +1,53 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
 
     public static void main(String[] args) {
-        /* Singleton
+        /*
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+         */
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
+        
+        /* #1. Singleton
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -19,6 +58,7 @@ public class Main {
         System.out.println(aClient.equals(bClient));
          */
 
+        /* #2. Adapter
         HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
 
@@ -31,6 +71,7 @@ public class Main {
         Electronic110V airAdapter = new SocketAdapter(airConditioner);
         //connect(airConditioner);
         connect(airAdapter);
+         */
     }
 
     // 콘센트
